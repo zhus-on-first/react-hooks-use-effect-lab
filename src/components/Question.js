b6e3626f577dbd1e88b8f9d3db9726664bd64da5
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-08-28 19:50:30 -0700

    lab complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (timeRemaining === 1) {
+        onAnswered(false);
+        setTimeRemaining(10);
+      } else {
+        setTimeRemaining(timeRemaining - 1);
+      }
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [onAnswered, timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -30,3 +41,12 @@ function Question({ question, onAnswered }) {
 }
 
 export default Question;
+
+// const timer = setTimeout(() => {
+//   if (timeRemaining === 1) {
+//     onAnswered(false);
+//     setTimeRemaining(10);
+//   } else {
+//     setTimeRemaining(timeRemaining - 1);
+//   }
+// }, 1000);
